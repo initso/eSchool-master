929c58a05175a170b4a669cc41eb3b70eeffeebe
pooja07061990 <pooja07061990@gmail.com [push] default = current>
2014-04-21 00:00:24 +0530

    Lecture summary new UI design Images added in image folder for lecture summary and header

--- app/controllers/lecture_summary.js
+++ app/controllers/lecture_summary.js
@@ -14,80 +14,92 @@ function createRow(subject, teacher, description, alerts, i) {
 	var tableRow = Ti.UI.createTableViewRow({
 		dataId : i,
 		className : 'row',
-		objName : 'row',
-		height : Alloy.Globals.Styles.TableViewRow.height,
+		objName : 'row'
 	});
-	var tabBackRow = Ti.UI.createView();
-
-	var tabRow = Ti.UI.createView({
+	var rowStyle = $.createStyle({
 		classes : 'tableRow'
 	});
-	var view1 = Ti.UI.createView();
-
-	var style = $.createStyle({
-		classes : 'subject'
+	tableRow.applyProperties(rowStyle);
+	
+	var tableRowBackgroundView = Ti.UI.createView({
+		classes : 'tableRowBackgoundView'
 	});
+	var rowBackgroundStyle = $.createStyle({
+		classes : 'tableRowBackgoundView'
+	});
+	tableRowBackgroundView.applyProperties(rowBackgroundStyle);
+	
+	var tableRowContentView = Ti.UI.createView({
+		classes : 'tableRowContentView'
+	});
+	var rowContentStyle = $.createStyle({
+		classes : 'tableRowContentView'
+	});
+	tableRowContentView.applyProperties(rowContentStyle);
 
 	var subjectLabel = Ti.UI.createLabel({
 		text : subject,
-		classes : 'subject'
+		classes : 'subjectLabel'
 	});
-
-	subjectLabel.applyProperties(style);
-
-	view1.add(subjectLabel);
+	var subjectStyle = $.createStyle({
+		classes : 'subjectLabel'
+	});
+	subjectLabel.applyProperties(subjectStyle);
+	
+	var timeContainerView = Ti.UI.createView({
+		classes : 'timeContainerView'
+	});
+	var styletimeContainer = $.createStyle({
+		classes : 'timeContainerView'
+	});
+	timeContainerView.applyProperties(styletimeContainer);
+	
+	var timeLabel = Ti.UI.createLabel({
+		text : '8.00 am - 8.30 am',
+		classes : 'timeLabel'
+	});
+	var styleTime = $.createStyle({
+		classes : 'timeLabel'
+	});
+	timeLabel.applyProperties(styleTime);
+	 
+	var teacherLabel = Ti.UI.createLabel({
+		text : teacher,
+		classes : 'teacherLabel'
+	});
+	var styleTeacher = $.createStyle({
+		classes : 'teacherLabel'
+	});
+	teacherLabel.applyProperties(styleTeacher);
+	
+	timeContainerView.add(timeLabel);
+	timeContainerView.add(teacherLabel);
+	
+	var lectureDetailLabel = Ti.UI.createLabel({
+		text: description,
+		classes : 'lectureDetailLabel'
+	});
+	var styleLectureDetail = $.createStyle({
+		classes : 'lectureDetailLabel'
+	});
+	lectureDetailLabel.applyProperties(styleLectureDetail);
+	
+	tableRowContentView.add(subjectLabel);
+	tableRowContentView.add(timeContainerView);
+	tableRowContentView.add(lectureDetailLabel);
+	tableRowBackgroundView.add(tableRowContentView);
+	tableRow.add(tableRowBackgroundView);
 
 	if (alerts) {
-		var style1 = $.createStyle({
-			classes : 'rightImage'
+		var rightImageStyle = $.createStyle({
+			classes : 'rowRightImageTest'
 		});
 
 		var rightImage = Ti.UI.createImageView();
-		rightImage.applyProperties(style1);
+		rightImage.applyProperties(rightImageStyle);
 
-		view1.add(rightImage);
-	}
-
-	var stylelecture = $.createStyle({
-		classes : 'lectureDetail'
-	});
-
-	var lectureDetail = Ti.UI.createView();
-	lectureDetail.applyProperties(stylelecture);
-
-	var style2 = $.createStyle({
-		classes : 'teacher'
-	});
-
-	var teacherLabel = Ti.UI.createLabel({
-		text : teacher
-	});
-	teacherLabel.applyProperties(style2);
-
-	var style4 = $.createStyle({
-		classes : 'description',
-		id : 'description1'
-	});
-
-	var description1Label = Ti.UI.createLabel({
-		text : description
-	});
-	description1Label.applyProperties(style4);
-
-	lectureDetail.add(teacherLabel);
-	lectureDetail.add(description1Label);
-
-	var style5 = $.createStyle({
-		classes : 'description',
-		id : 'description2'
-	});
-
-
-	tabRow.add(view1);
-	tabRow.add(lectureDetail);
-
-	tabBackRow.add(tabRow);
-	tableRow.add(tabBackRow);
+		tableRow.add(rightImage);
+	};
 
 	console.log(i);
 	// Resource Clean-Up
@@ -95,7 +107,7 @@ function createRow(subject, teacher, description, alerts, i) {
 	// Finished
 	return tableRow;
 }
-
+		
 function actionDatePick(e) {
 	var picker = Ti.UI.createPicker({
 		type : Ti.UI.PICKER_TYPE_DATE,
